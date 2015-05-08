61baaf87e6b59b1f75cf8200c0ebd7d5b534268d
Jeff Lyon <jeff@rubbingalcoholic.com>
2015-05-08 15:21:59 -0700

    initial checkin

---
+++ js/usafreedom.js
@@ -0,0 +1,23 @@
+var fb = document.querySelectorAll('a.facebook');
+for (var i = 0; i < fb.length; i++) {
+    fb[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        window.open('https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.usafreedom.fail%2F');
+    }, false);
+}
+
+var tws = document.querySelectorAll('a.twitter');
+for (var i = 0; i < tws.length; i++) {
+    tws[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        window.open('https://twitter.com/intent/tweet?text='+encodeURIComponent(TWEET_TEXT));
+    }, false);
+}
+
+var ems = document.querySelectorAll('a.email');
+for (var i = 0; i < ems.length; i++) {
+    ems[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        window.location.href = 'mailto:?subject='+encodeURIComponent(EMAIL_SUBJECT)+'&body=https%3A%2F%2Fwww.usafreedom.fail%2F';
+    }, false);
+}
