207a7fc3ae71005da2d4ef296abe48d8daee96bd
Jeff Lyon <jeff@rubbingalcoholic.com>
2015-05-08 15:39:16 -0700

    no https for now

--- js/usafreedom.js
+++ js/usafreedom.js
@@ -2,7 +2,7 @@ var fb = document.querySelectorAll('a.facebook');
 for (var i = 0; i < fb.length; i++) {
     fb[i].addEventListener('click', function(e) {
         e.preventDefault();
-        window.open('https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.usafreedom.fail%2F');
+        window.open('https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.usafreedom.fail%2F');
     }, false);
 }
 
@@ -18,6 +18,6 @@ var ems = document.querySelectorAll('a.email');
 for (var i = 0; i < ems.length; i++) {
     ems[i].addEventListener('click', function(e) {
         e.preventDefault();
-        window.location.href = 'mailto:?subject='+encodeURIComponent(EMAIL_SUBJECT)+'&body=https%3A%2F%2Fwww.usafreedom.fail%2F';
+        window.location.href = 'mailto:?subject='+encodeURIComponent(EMAIL_SUBJECT)+'&body=http%3A%2F%2Fwww.usafreedom.fail%2F';
     }, false);
 }
